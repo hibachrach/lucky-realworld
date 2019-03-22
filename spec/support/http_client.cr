de69598d46583edcf7c288f6b0dbfb6549ce0c59
Harry Bachrach <harry@docsend.com>
2019-03-21 22:14:10 -0700

    Enable client to make requests while signed in

--- spec/support/http_client.cr
+++ spec/support/http_client.cr
@@ -1,25 +1,36 @@
 class HTTPClient < Lucky::BaseHTTPClient
+  private getter persistent_headers = HTTP::Headers{ "Content-Type" => "application/json" }
+
   def initialize(port = ENV["TEST_PORT"].to_i)
     @client = HTTP::Client.new(Lucky::Server.settings.host, port: port)
   end
 
+  def sign_in!(as user)
+    jwt = JwtService.token(user)
+    persistent_headers.add("Authorization", "Token #{jwt}")
+  end
+
+  def sign_out!
+    persistent_headers.delete("Authorization")
+  end
+
   def get(path : String, headers : HTTP::Headers? = nil, params : Object? = nil)
     body = params.try(&.to_json)
-    @client.get(path, headers: set_as_json(headers), body: body)
+    @client.get(path, headers: add_persistent_headers(headers), body: body)
   end
 
   {% for method in [:put, :patch, :post] %}
 
     def {{method.id}}(path : String, body : Object, headers : HTTP::Headers? = nil)
       body = body.try(&.to_json)
-      @client.{{method.id}}(path, headers: set_as_json(headers), body: body)
+      @client.{{method.id}}(path, headers: add_persistent_headers(headers), body: body)
     end
 
   {% end %}
 
-  private def set_as_json(headers : HTTP::Headers? = nil)
-    headers = headers || HTTP::Headers.new
-    headers["Content-Type"] = "application/json"
+  private def add_persistent_headers(headers : HTTP::Headers? = nil)
+    headers = headers.dup || HTTP::Headers.new
+    headers.merge!(persistent_headers)
     headers
   end
 end
